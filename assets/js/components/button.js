6781e4cd54460b242e7d66762e7faa9431fe97dc
Gabriela Carniel <gabiicarniel@gmail.com>
2022-11-28 18:17:12 -0300

    changes

--- assets/js/components/button.js
+++
@@ -1,8 +0,0 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.menu-fechado')
-    if(menuMobile.classList.contains('open')){
-        menuMobile.classList.remove('open')
-    } else {
-        menuMobile.classList.add('open')
-    }
-}
